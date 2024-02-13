7cca6d9eafe2d055e7e9bf441bb0c4a8cd940c1e
msifatsadiq <msifatsadiq@gmail.com>
2024-02-13 16:13:21 +0600

    calculate the base and height of the rectangle area and display it in the area section

--- JS Script/rectengular-calc.js
+++ JS Script/rectengular-calc.js
@@ -3,12 +3,23 @@ function calculateRectangularArea() {
     const rectangleBaseInput = document.getElementById('rectangle-base');
     const rectangleBaseValue = rectangleBaseInput.value;
     const rectangleBaseValueToNumber = parseFloat(rectangleBaseValue);
-    console.log(typeof rectangleBaseValueToNumber);
+    console.log(rectangleBaseValueToNumber);
 
     // rectangle height value
     const rectangleHeightInput = document.getElementById('rectangle-height');
     const rectangleHeightValue = rectangleHeightInput.value;
     const rectangleHeightValueToNumber = parseFloat(rectangleHeightValue);
-    console.log(typeof rectangleHeightValueToNumber);
+    console.log(rectangleHeightValueToNumber);
+
+    // calculate the base and height of the area
+
+    const calculateRectangleArea = rectangleBaseValueToNumber * rectangleHeightValueToNumber;
+    console.log('Area of the rectangle is: ', calculateRectangleArea);
+
+    // display the rectangle area
+
+    const rectangleAreaSpan = document.getElementById('rectangle-span');
+    rectangleAreaSpan.innerText = calculateRectangleArea;
+
 
 }
