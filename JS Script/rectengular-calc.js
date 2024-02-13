5ceea32fe8e5f645cc9ed549b2fb455a95ae8e27
msifatsadiq <msifatsadiq@gmail.com>
2024-02-13 17:44:57 +0600

    adding the different and short way of making the rectangle calculation

--- JS Script/rectengular-calc.js
+++ JS Script/rectengular-calc.js
@@ -11,15 +11,21 @@ function calculateRectangularArea() {
     const rectangleHeightValueToNumber = parseFloat(rectangleHeightValue);
     console.log(rectangleHeightValueToNumber);
 
-    // calculate the base and height of the area
-
+    // calculate the base and height of the rectangle area
     const calculateRectangleArea = rectangleBaseValueToNumber * rectangleHeightValueToNumber;
     console.log('Area of the rectangle is: ', calculateRectangleArea);
 
     // display the rectangle area
-
     const rectangleAreaSpan = document.getElementById('rectangle-span');
     rectangleAreaSpan.innerText = calculateRectangleArea;
+}
 
+// another short way to do this calculation 
+function calculateRectangularArea() {
+    let rectangleBaseInput = parseFloat(document.getElementById('rectangle-base').value);
+    let rectangleHeightInput = parseFloat(document.getElementById('rectangle-height').value);
+    console.log(rectangleBaseInput, rectangleHeightInput);
+    let value = document.getElementById('rectangle-span').innerText = rectangleBaseInput * rectangleHeightInput;
+    console.log(value);
 
 }
