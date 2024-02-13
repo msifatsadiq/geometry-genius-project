316b0fcfe6301ae1318c155f7dd1e5a1363c823c
msifatsadiq <msifatsadiq@gmail.com>
2024-02-13 15:44:10 +0600

    adding the rectangle card with adding input,value

---
+++ JS Script/rectengular-calc.js
@@ -0,0 +1,12 @@
+function calculateRectangularArea() {
+    // rectangle base value 
+    const rectangleBaseInput = document.getElementById('rectangle-base');
+    const rectangleBaseValue = rectangleBaseInput.value;
+    console.log(rectangleBaseValue);
+
+    // rectangle height value
+    const rectangleHeightInput = document.getElementById('rectangle-height');
+    const rectangleHeightValue = rectangleHeightInput.value;
+    console.log(rectangleHeightValue);
+
+}
